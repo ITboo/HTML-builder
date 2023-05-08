c10ce95e6f6f4d004eee13d36a22ae2b3d4541eb
ITboo <annaacdc@tut.by>
2023-05-08 17:16:23 +0300

    fix: fix file-listing

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,20 +1,26 @@
-import { readdir } from 'fs/promises';
+import { readdir, stat } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { stdout } from 'process';
 import { errorMsg } from '../constants/messages.js';
 
-export const list = async () => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dirPath = path.join(__dirname, 'secret-folder');
 
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
+readdir(dirPath, (_, files) => {
+    files.forEach((file) => {
+        stat(path.resolve(dirPath, file), (_, stats) => {
+            if (stats.isFile()) {
+                const fileName = path.parse(file).name
+                const fileExtension = path.parse(file).ext.slice(1)
+                const fileSize = Math.floor(stats.size / 1024);
 
-    const dirPath = path.join(__dirname, 'secret-folder');
-
-    const listFile = await readdir(dirPath)
-        .catch(() => {
-            throw new Error(errorMsg)
-        });
-    console.log(listFile);
-}
-
-await list();
+                stdout.write(`${fileName} - ${fileExtension} - ${fileSize} kb\n`)
+            }
+        })
+    })
+    if (!dirPath) {
+        throw new Error(errorMsg)
+    }
+})
